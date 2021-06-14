4486bb35836b52b0ce6ac98af563784d3768b728
Geanderson Lemonte <geandersonlemonte.gl@gmail.com>
2021-06-14 09:37:55 -0300

    Initial Commit

---
+++ Chuck Norris/Chuck_NorrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chuck_NorrisApp.swift
+//  Chuck Norris
+//
+//  Created by Geanderson Lemonte on 14/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Chuck_NorrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
